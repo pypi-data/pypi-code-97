c9e8f902156484ea336b8d7e8c77ef8efe0bd504
Tom Forbes <tom@tomforb.es>
2021-10-02 05:11:26 +0000

    {"name":"the-rational-male-zh","version":"2021.10.2.0","file":"the_rational_male_zh-2021.10.2.0-py3-none-any.whl","path":"the-rational-male-zh/the_rational_male_zh-2021.10.2.0-py3-none-any.whl"}

---
+++ the-rational-male-zh/the_rational_male_zh-2021.10.2.0-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""the-rational-male-zh
+https://github.com/apachecn/the-rational-male-zh"""
+
+__author__ = "ApacheCN"
+__email__ = "apachecn@163.com"
+__license__ = "CC BY-NC-SA 4.0"
+__version__ = "2021.10.2.0"

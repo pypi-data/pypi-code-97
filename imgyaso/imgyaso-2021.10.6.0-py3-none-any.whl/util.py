6babe08993f31b858bdbeeac3502b4154bd33a07
Tom Forbes <tom@tomforb.es>
2021-10-06 07:38:10 +0000

    {"name":"imgyaso","version":"2021.10.6.0","file":"imgyaso-2021.10.6.0-py3-none-any.whl","path":"imgyaso/imgyaso-2021.10.6.0-py3-none-any.whl"}

---
+++ imgyaso/imgyaso-2021.10.6.0-py3-none-any.whl/util.py
@@ -0,0 +1,18 @@
+import subprocess as subp
+import tempfile
+import uuid
+import os
+from os import path
+from PIL import Image
+from io import BytesIO
+
+def is_svg(img):
+    return img[:5] == b'<?xml'
+
+def conv2png(img):
+    if img[:4] == b'\x89PNG':
+        return img
+    img = Image.open(BytesIO(img))
+    bio = BytesIO()
+    img.save(bio, 'png')
+    return bio.getvalue()
